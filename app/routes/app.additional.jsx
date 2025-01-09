cafddf387243e17168fb6103cee7d24851525ab1
harryevm <97522066+harryevm@users.noreply.github.com>
2025-01-09 10:43:18 +0530

    Update app.additional.jsx

--- app/routes/app.additional.jsx
+++ app/routes/app.additional.jsx
@@ -61,6 +61,7 @@ export default function AdditionalPage() {
                 .
               </Text>
               <Text as="p" variant="bodyMd">
+                assdadssd
                 To create your own page and have it show up in the app
                 navigation, add a page inside <Code>app/routes</Code>, and a
                 link to it in the <Code>&lt;NavMenu&gt;</Code> component found
