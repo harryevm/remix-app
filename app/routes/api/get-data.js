e42e0ea0ba8e15846b5df3f03b1c2f5d34a42bfb
harryevm <97522066+harryevm@users.noreply.github.com>
2025-01-09 11:39:45 +0530

    Update get-data.js

--- app/routes/api/get-data.js
+++ app/routes/api/get-data.js
@@ -3,12 +3,6 @@ import { json } from "@remix-run/node";
 import { fetchMongoData } from "../../entry.server";
 
 
-export async function loader() {
-  try {
-    const data = await fetchMongoData();
-    return json(data); // Return data as JSON
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return json({ error: "Failed to fetch data" }, { status: 500 });
-  }
+export function loader() {
+  return new Response('API Test is working!');
 }
