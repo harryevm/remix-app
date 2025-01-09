e29e2ff544bb51c7187acb787916b64aaf4ccb0a
harryevm <97522066+harryevm@users.noreply.github.com>
2025-01-09 11:19:53 +0530

    Update api.getdata.jsx

--- app/routes/api.getdata.jsx
+++ app/routes/api.getdata.jsx
@@ -7,7 +7,7 @@ import { fetchMongoData } from "../entry.server";
 export async function loader() {
   try {
     const data = await fetchMongoData();
-    return json(data); // Return data as JSON
+    return data; // Return data as JSON
   } catch (error) {
     console.error("Error fetching data:", error);
     return json({ error: "Failed to fetch data" }, { status: 500 });
