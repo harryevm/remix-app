10f6677e30ddc1b5c34d2e62be2792af79f25643
harryevm <97522066+harryevm@users.noreply.github.com>
2025-01-24 16:22:24 +0530

    Update user.$userId.jsx

--- app/routes/user.$userId.jsx
+++ app/routes/user.$userId.jsx
@@ -4,18 +4,17 @@ import { useLoaderData, useParams } from '@remix-run/react';
 import { fetchMongoDataById } from '../entry.server';
 
 export const loader = async ({ params }) => {
-    // const { userId } = params; // Destructure userId from params
-    const { userId } = useParams();
+    const { userId } = params; // Destructure userId from params
     
-    console.log(userId);
+    console.log(userId); // Log userId to check if it's correct
     
-     const data = await fetchMongoDataById(userId); // Use userId instead of 'item'
-  
+    const data = await fetchMongoDataById(userId); // Use userId to fetch data
+    
     if (!data) {
       throw new Response('Item not found', { status: 404 });
     }
   
-    return { data };
+    return { data }; // Return the fetched data
   };
   
 
