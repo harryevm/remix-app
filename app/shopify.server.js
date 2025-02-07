b9a1d1189b8f420940231c4c60e460814dc2e61a
harryevm <97522066+harryevm@users.noreply.github.com>
2025-02-07 13:46:48 +0530

    Update shopify.server.js

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -4,18 +4,8 @@ import {
   AppDistribution,
   shopifyApp,
 } from "@shopify/shopify-app-remix/server";
-// import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
+import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import prisma from "./db.server";
-import { MongoDBSessionStorage } from "@shopify/shopify-app-session-storage-mongodb";
-import { MongoClient } from "mongodb";
-const url =
-  "mongodb+srv://harish_c:harish_c@cluster0.kdyad.mongodb.net/?retryWrites=true&w=majority";
-
-const client = new MongoClient(url);
-await client.connect();
-const db = client.db("Trevor");
-const mongoSessionStorage = new MongoDBSessionStorage(db, "sessions");
-
 
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
@@ -24,8 +14,7 @@ const shopify = shopifyApp({
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  // sessionStorage: new PrismaSessionStorage(prisma),
-  sessionStorage: mongoSessionStorage,
+  sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
   future: {
     unstable_newEmbeddedAuthStrategy: true,
