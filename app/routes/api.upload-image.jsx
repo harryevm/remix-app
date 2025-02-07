a036905af67b34b1d37a702aade8e074a87f9fb2
harryevm <97522066+harryevm@users.noreply.github.com>
2025-02-07 14:33:22 +0530

    Update api.upload-image.jsx

--- app/routes/api.upload-image.jsx
+++ app/routes/api.upload-image.jsx
@@ -42,8 +42,7 @@ export async function action({ request }) {
   }
 
   try {
-    console.log(shopify)
-    console.log(shopify.authenticate)
+    
     console.log(request)
     
     const formData = await request.formData();
