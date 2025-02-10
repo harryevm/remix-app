42bfd1a7f817fa3fa4a6b011881b2bd5c0ac60fa
harryevm <97522066+harryevm@users.noreply.github.com>
2025-02-10 15:11:03 +0530

    Update user.$userId.jsx

--- app/routes/user.$userId.jsx
+++ app/routes/user.$userId.jsx
@@ -1,3 +1,10 @@
+import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
+import { boundary } from "@shopify/shopify-app-remix/server";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { NavMenu } from "@shopify/app-bridge-react";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
+import { authenticate } from "../shopify.server";
+
 import { useLoaderData } from '@remix-run/react';
 import { fetchMongoDataById } from '../entry.server';
 export const links = () => [{ rel: "stylesheet", href: polarisStyles },{ rel: "stylesheet", href: "/style.css" }];
