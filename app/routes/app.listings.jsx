16c27aa2a6e342e8d252a35be4f5386b510f83f9
harryevm <97522066+harryevm@users.noreply.github.com>
2025-02-14 08:47:06 +0000

    App.listings.jsx

--- app/routes/app.listings.jsx
+++ app/routes/app.listings.jsx
@@ -2,21 +2,9 @@ import { TitleBar } from "@shopify/app-bridge-react";
 import { json } from "@remix-run/node";
 import { useEffect, useState } from "react";
 import { Link, useLoaderData } from "@remix-run/react";
-import { totalPropertyCount, totalUserCount } from "../entry.server";
-
-export async function loader() {
-  const [userCount, propertyCount] = await Promise.all([
-    totalUserCount(),
-    totalPropertyCount(),
-  ]);
-
-  return json({ userCount, propertyCount });
-}
-
 
 export default function ListingPage() {
-  const { userCount, propertyCount } = useLoaderData();
-  // const { connected, message } = useLoaderData();
+
   const [sampleData, setSampleData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -52,7 +40,6 @@ export default function ListingPage() {
     setCheckboxState(initialState);
   }, []);
 
-  console.log(visibleColumns)
 
   const handleCheckboxChange = (e) => {
     const { value, checked } = e.target;
@@ -88,24 +75,12 @@ export default function ListingPage() {
     
   }, []);
 
-  console.log(sampleData)
-
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error: {error}</div>;
 
   return (
     <>
-    <p>Total Unique Users: {userCount}</p>
-    <p>Total Properties: {propertyCount}</p>
-           <div class="dashboard">
-	
-    {/* <div class="sidebar">
-        <a href=""><h1>NYFISBO</h1></a>
-        <ul class="menu">
-            <li id="index-link"><a href="index.html"><svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M0 3.5194C0 1.67845 1.49239 0.186066 3.33333 0.186066H12.6667C14.5076 0.186066 16 1.67845 16 3.5194V12.8527C16 14.6937 14.5076 16.1861 12.6667 16.1861H3.33333C1.49239 16.1861 0 14.6937 0 12.8527V3.5194ZM3.33333 1.5194C2.22876 1.5194 1.33333 2.41483 1.33333 3.5194V4.18607H7.33333V1.5194H3.33333ZM8.66667 1.5194V10.8527H14.6667V3.5194C14.6667 2.41483 13.7712 1.5194 12.6667 1.5194H8.66667ZM14.6667 12.1861H8.66667V14.8527H12.6667C13.7712 14.8527 14.6667 13.9573 14.6667 12.8527V12.1861ZM7.33333 14.8527V5.5194H1.33333V12.8527C1.33333 13.9573 2.22876 14.8527 3.33333 14.8527H7.33333Z" fill="#222222"/> </svg>Dashboard</a></li>
-            <li id="listing-link" ><a href="listing.html"><svg width="19" height="21" viewBox="0 0 19 21" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M9.29035 0.686096C9.70456 0.686096 10.0403 1.02189 10.0403 1.4361V2.1861H11.5389C12.7815 2.1861 13.7889 3.19345 13.7889 4.4361V8.1861H16.0389C17.2815 8.1861 18.2889 9.19345 18.2889 10.4361V17.9361C18.2889 19.1787 17.2815 20.1861 16.0389 20.1861H5.53888C5.53838 20.1861 5.53937 20.1861 5.53888 20.1861H2.53888C1.29623 20.1861 0.288879 19.1787 0.288879 17.9361V10.8762C0.288879 10.1137 0.675024 9.40315 1.31477 8.98834L4.31477 7.04314C4.46863 6.94338 4.62755 6.86523 4.78888 6.80722V4.4361C4.78888 3.19345 5.79623 2.1861 7.03888 2.1861H8.54035V1.4361C8.54035 1.02189 8.87613 0.686096 9.29035 0.686096ZM6.28888 6.80905C7.13203 7.11013 7.78888 7.90269 7.78888 8.93101V18.6861H10.7889V10.4361C10.7889 9.45643 11.415 8.623 12.2889 8.31412V4.4361C12.2889 4.02189 11.9531 3.6861 11.5389 3.6861H7.03888C6.62467 3.6861 6.28888 4.02189 6.28888 4.4361V6.80905ZM13.0389 9.6861C12.6247 9.6861 12.2889 10.0219 12.2889 10.4361V18.6861H16.0389C16.453 18.6861 16.7889 18.3502 16.7889 17.9361V10.4361C16.7889 10.0219 16.453 9.6861 16.0389 9.6861H13.0389ZM5.13085 8.30172L2.13085 10.2469C1.91759 10.3852 1.78888 10.6221 1.78888 10.8762V17.9361C1.78888 18.3502 2.12467 18.6861 2.53888 18.6861H6.28888V8.93101C6.28888 8.33632 5.62982 7.97818 5.13085 8.30172Z" fill="#222222"/> </svg>Property Listing</a></li>
-        </ul>
-    </div> */}
+    <div class="dashboard">
     <div class="dashboard-info">
         <div class="dashboard-header">
             <a href=""><h1>NYFISBO</h1></a>
