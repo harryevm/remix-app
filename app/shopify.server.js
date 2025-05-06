52384f3e011b9ae7ff00c99b73b4cfa9a2120a36
harryevm <97522066+harryevm@users.noreply.github.com>
2025-05-06 10:21:57 +0530

    Update shopify.server.js

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -39,7 +39,7 @@ const shopify = shopifyApp({
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
     : {}),
-});
+}); 
 
 export default shopify;
 export const apiVersion = ApiVersion.October24;
