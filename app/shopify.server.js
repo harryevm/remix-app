a8d5a16ecfa018016c388dad1767132d7319542c
harryevm <97522066+harryevm@users.noreply.github.com>
2025-05-06 16:18:10 +0530

    Revert "Update shopify.server.js"
    This reverts commit 52384f3e011b9ae7ff00c99b73b4cfa9a2120a36.

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -45,7 +45,7 @@ const shopify = shopifyApp({
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
     : {}),
-}); 
+});
 
 export default shopify;
 export const apiVersion = ApiVersion.January25;
