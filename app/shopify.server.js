a54fa40c91f15768cb793283f89e5e25cee82572
harryevm <97522066+harryevm@users.noreply.github.com>
2025-05-06 17:43:18 +0530

    last-commit

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -4,32 +4,32 @@ import {
   AppDistribution,
   shopifyApp,
 } from "@shopify/shopify-app-remix/server";
-import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
+
 import prisma from "./db.server";
-import { MongoDBSessionStorage } from "@shopify/shopify-app-session-storage-mongodb";
+import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 // import { MemorySessionStorage } from "@shopify/shopify-app-session-storage-memory";
-const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://harish_c:harish_c@cluster0.kdyad.mongodb.net/?retryWrites=true&w=majority";
+// const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://harish_c:harish_c@cluster0.kdyad.mongodb.net/?retryWrites=true&w=majority";
 
  
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.October24,
-  scopes: process.env.SCOPES?.split(",") || "read_products,write_products,write_files,read_files,read_themes,write_themes",
+  scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
-  // sessionStorage: new PrismaSessionStorage(prisma),
+  sessionStorage: new PrismaSessionStorage(prisma),
   // sessionStorage: new MongoDBSessionStorage(
   //   'mongodb+srv://harish_c:harish_c@cluster0.kdyad.mongodb.net/?retryWrites=true&w=majority',
   //   'test',  // Replace with your actual database name
   // ),
   // sessionStorage: new MemorySessionStorage(),
-  sessionStorage: new MongoDBSessionStorage(MONGO_URI, "Trevor", {
-    cookieOptions: {
-      sameSite: "None",
-      secure: false,
-    },
-  }),
+  // sessionStorage: new MongoDBSessionStorage(MONGO_URI, "Trevor", {
+  //   cookieOptions: {
+  //     sameSite: "None",
+  //     secure: false,
+  //   },
+  // }),
 
   distribution: AppDistribution.AppStore,
   future: {
