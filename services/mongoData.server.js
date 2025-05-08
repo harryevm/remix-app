d2c08bf666c80d49d8175fac89a054df2c55c8a4
harryevm <97522066+harryevm@users.noreply.github.com>
2025-05-08 16:54:50 +0530

    a

--- services/mongoData.server.js
+++ services/mongoData.server.js
@@ -2,7 +2,7 @@
 import prisma from "../app/db.server";
 
 export async function fetchMongoData(limit = null) {
-  return await prisma.trevor.findMany({
+  return await prisma.Trevor.findMany({
     orderBy: {
       createdAt: "desc",
     },
@@ -11,13 +11,13 @@ export async function fetchMongoData(limit = null) {
 }
 
 export async function fetchMongoDataById(id) {
-  // return await prisma.trevor.findUnique({
+  // return await prisma.Trevor.findUnique({
   //   where: { id }, // 'id' should be a string (MongoDB ObjectId as string)
   // });
 }
 
 export async function insertMongoData(data) {
-  return await prisma.trevor.create({
+  return await prisma.Trevor.create({
     data: {
       ...data,
       createdAt: new Date(),
@@ -26,11 +26,11 @@ export async function insertMongoData(data) {
 }
 
 export async function totalPropertyCount() {
-  return await prisma.trevor.count();
+  return await prisma.Trevor.count();
 }
 
 export async function totalUserCount() {
-  const grouped = await prisma.trevor.findMany({
+  const grouped = await prisma.Trevor.findMany({
     select: { email: true },
   });
   const uniqueEmails = new Set(grouped.map(r => r.email));
