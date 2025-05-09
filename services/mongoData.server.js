f1879b063688d628454b47f51c0393cfe2e8931d
harryevm <97522066+harryevm@users.noreply.github.com>
2025-05-09 10:18:42 +0530

    C

--- services/mongoData.server.js
+++ services/mongoData.server.js
@@ -2,35 +2,20 @@
 import prisma from "../app/db.server";
 
 export async function fetchMongoData(limit = null) {
-  return await prisma.Trevor.findMany({
+  return await prisma.trevor.findMany({
     take: limit ?? undefined,
   });
 }
 
 
 export async function fetchMongoDataById(id) {
-  // return await prisma.Trevor.findUnique({
+  // return await prisma.trevor.findUnique({
   //   where: { id }, // 'id' should be a string (MongoDB ObjectId as string)
   // });
 }
 
-export async function insertMongoData(data) {
-  return await prisma.trevor.create({
-    data: {
-      ...data,
-      mediaReleaseDate: data.mediaReleaseDate ? new Date(data.mediaReleaseDate) : null,  // Handle DateTime conversion
-      agencyDate: data.agencyDate ? new Date(data.agencyDate) : null,
-      fairHousingDate: data.fairHousingDate ? new Date(data.fairHousingDate) : null,
-      propertyDate: data.propertyDate ? new Date(data.propertyDate) : null,
-      listingDate: data.listingDate ? new Date(data.listingDate) : null,
-      createdAt: new Date(),  // Current date for createdAt
-      lotUnit: data.lotUnit || null,  // Handle missing lotUnit
-      garageSpecify: data.garageSpecify || null,  // Handle missing garageSpecify
-      basement: data.basement || null,  // Handle missing basement
-      outdoorFeatures: data.outdoorFeatures || null,  // Handle missing outdoorFeatures
-      additionalFeatures: data.additionalFeatures || null,  // Handle missing additionalFeatures
-    },
-  });
+export function insertMongoData(data) {
+  return prisma.trevor.create({ data });
 }
 
 
@@ -38,11 +23,11 @@ export async function insertMongoData(data) {
 
 
 export async function totalPropertyCount() {
-  return await prisma.Trevor.count();
+  return await prisma.trevor.count();
 }
 
 export async function totalUserCount() {
-  const grouped = await prisma.Trevor.findMany({
+  const grouped = await prisma.trevor.findMany({
     select: { email: true },
   });
   const uniqueEmails = new Set(grouped.map(r => r.email));
